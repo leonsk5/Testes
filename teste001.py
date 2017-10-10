c38785088e268204b58cf9a29830e643172aaa87
leonsk5 <leonsk.55@gmail.com>
2017-10-10 08:57:09 -0300

    Update teste001.py

--- teste001.py
+++ teste001.py
@@ -1,4 +1,4 @@
 nome=input('Qual é o seu nome? ')
 idade=input('Qual é sua idade? ')
 peso=input('Qual é o seu peso? ')
-print(nome, idade, peso)
+print('Seja bem-vindo! {},\n Sua idade é {},\n Seu peso é {} ' .format(nome, idade, peso))
