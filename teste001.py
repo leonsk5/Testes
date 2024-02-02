ff5c96dbcd14833378b566ba6b6bda607f0cf361
Leonardo S Melo <leonsk.55@gmail.com>
2024-02-02 19:21:35 -0300

    Delete teste001.py

--- teste001.py
+++
@@ -1,5 +0,0 @@
-#programa para recepicionar o cliente
-nome = str(input('Qual é o seu nome? '))
-idade = int(input('Qual é sua idade? '))
-peso = int(input('Qual é o seu peso? '))
-print('Seja bem-vindo! {},\n Sua idade é {},\n Seu peso é {} ' .format(nome, idade, peso))
